a553280b3ede3d81ce71c30538b67b86138ba714
yuki <yuki.laurentiu@gmail.com>
2023-04-16 17:57:05 -0600

    added images folder and sets assets image

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,7 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w='),
+            image: AssetImage('images/diamond1.png'),
           ),
         ),
       ),
