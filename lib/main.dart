06f9566b37524a65a6071adeb4714bbe0c8255fe
yuki <yuki.laurentiu@gmail.com>
2023-04-17 09:46:24 -0600

    changed image

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,12 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: const Center(
-              child: Text('I Am Rich')
-          ),
+          title: const Center(child: Text('I Am Rich')),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: const Center(
           child: Image(
-            image: AssetImage('images/diamond.png'),
+            image: AssetImage('images/diamond2.png'),
           ),
         ),
       ),
